a2fa9c7512b5341470cadd94e7c39dfc53f1710a
anshumanawasthi214 <awasthiengineer214@gmail.com>
2025-11-13 16:32:12 +0530

    Most optimized versin without using regex

--- Two Pointers Pattern Based Questions/PalindromeList.java
+++ Two Pointers Pattern Based Questions/PalindromeList.java
@@ -1,31 +1,38 @@
 public class PalindromeList {
-    public static boolean checkPalindrome(String s){
-        int left=0;
-        int right=s.length()-1;
 
-        while(left<right){
-            if(s.charAt(left)==s.charAt(right)){
+    public static boolean checkPalindrome(String s){
+        int left = 0, right = s.length() - 1;
+
+        while(left < right){
+            // Skip non-alphanumeric characters from the left
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
                 left++;
+            }
+
+            // Skip non-alphanumeric characters from the right
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
                 right--;
             }
-            else{
-                
+
+            // Compare characters (case-insensitive)
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
                 return false;
             }
-        }
-                  return true;
-                       
 
+            left++;
+            right--;
+        }
+
+        return true;
     }
-    public static void main(String args[]){
-        String s="    raCE  car";
-        s=s.replaceAll("[^a-zA-Z0-9]","");// it removes extra spaces completely means:     "  race      a   car "----> "raceacar"
-        s=s.toLowerCase(); //converts the string in lowercase
+
+    public static void main(String[] args){
+        String s = "    raCE.   car,";
         
-       if(checkPalindrome(s)){
-         System.out.println("String is palindrome");
-       }else{
-        System.out.println("String is not palindrome");
-       }
+        if(checkPalindrome(s)){
+            System.out.println("String is palindrome");
+        } else {
+            System.out.println("String is not palindrome");
+        }
     }
 }
