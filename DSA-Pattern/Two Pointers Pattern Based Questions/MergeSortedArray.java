179bd716a1f46ac161fe9df5728ddab9437d4192
anshumanawasthi214 <awasthiengineer214@gmail.com>
2025-11-21 23:43:04 +0530

    added merge sorted array

--- DSA-Pattern/Two Pointers Pattern Based Questions/MergeSortedArray.java
+++ DSA-Pattern/Two Pointers Pattern Based Questions/MergeSortedArray.java
@@ -1,33 +1,40 @@
 public class MergeSortedArray{
 
-    public static void merge(int nums1[],int nums2[],int n,int m){
-        int i=n-1;
-        int j=m-1;
-        int k=nums1.length-1;
-
-        while(i<m && j<n){
-                if(nums1[i]<nums2[j]){
-                    nums1[k++]=nums1[i++];
-                }
-                else if(nums1[i]==nums2[j]){
-                    nums1[k++]=nums1[i++];
-                }
-                else{
-                    nums1[k++]=nums2[j++];
-                }
+    public static void merge(int nums1[],int nums2[],int m,int n){
+       int k=(m+n)-1;
+       int i=m-1;
+       int j=n-1;
+      while(i>=0 && j>=0){
+        if(nums1[i]>=nums2[j]){
+            nums1[k]=nums1[i];
+           
+            i--;
         }
-        for(int a:nums1){
-            System.out.print(a+" ");
+        else{
+            nums1[k]=nums2[j];
+           
+            j--;
         }
+         k--;
+      }
 
+      while(j>=0){
+        nums1[k--]=nums2[j--];
+      }
+        
+
+       for(int e:nums1){
+        System.out.print(e+" ");
+       }
 
     }
     public static void main(String[] args) {
-        int arr[]={1,2,3,0,0,0};
+        int arr[]={0,0,0};
         int arr2[]={2,5,6};
 
-        int n=3;
-        int m=arr2.length;
-        merge(arr,arr2,n,m);
+        
+        int m=0;
+        int n=arr2.length;
+        merge(arr, arr2, m, n);
     }
 }
