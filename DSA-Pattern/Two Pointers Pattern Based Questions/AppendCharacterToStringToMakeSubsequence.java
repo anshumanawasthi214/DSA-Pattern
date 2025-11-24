7d56a01d47a9aba7ab8acf0b9bf1e9d2baf17b5f
anshumanawasthi214 <awasthiengineer214@gmail.com>
2025-11-24 14:01:58 +0530

    added one more program in two pointer folder which checks how many characters from string t  are required to append to string s to make String t a subsequence of String s

---
+++ DSA-Pattern/Two Pointers Pattern Based Questions/AppendCharacterToStringToMakeSubsequence.java
@@ -0,0 +1,21 @@
+public class AppendCharacterToStringToMakeSubsequence{
+
+    public static int appendCharacters(String s, String t) {
+        int left=0;
+        int right=0;
+        while(left<s.length() && right<t.length()){
+            if(s.charAt(left)==t.charAt(right)){
+                right++;
+            }
+                left++;
+        }
+        return t.length()-right;
+    }
+    public static void main(String[] args) {
+        String s="coaching";
+        String t="coding";
+
+        int numberOfCharacter=appendCharacters(s,t);
+        System.out.println(" minimum number of characters that need to be appended "+numberOfCharacter);
+    }
+}
