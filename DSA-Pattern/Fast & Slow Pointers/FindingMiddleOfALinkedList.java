22e15212a444c8e873111c124f3a36b1b5cc9566
anshumanawasthi214 <awasthiengineer214@gmail.com>
2025-11-26 13:39:55 +0530

    added finding middle of linked list program

---
+++ DSA-Pattern/Fast & Slow Pointers/FindingMiddleOfALinkedList.java
@@ -0,0 +1,43 @@
+public class FindingMiddleOfALinkedList {
+      static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+
+    }
+
+    public static void findMid(Node head){
+        Node slow=head;
+        Node fast=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+
+        System.out.println("Mid of the linked list is : "+slow.data);
+        
+    }
+    public static void main(String[] args) {
+       
+          // Creating nodes
+        Node head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+        Node fifth = new Node(5);
+
+        // Linking nodes like 1 -> 2 -> 3 -> 4 -> 5
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+
+        findMid(head);
+    
+
+    }
+}
