c7798d8be77ab89ba387144dc4d32b22b8164cdd
anshumanawasthi214 <awasthiengineer214@gmail.com>
2025-12-11 22:12:09 +0530

    added intervals first problem

--- DSA-Pattern/Interval Pattern/MergeInterval.java
+++ DSA-Pattern/Interval Pattern/MergeInterval.java
@@ -3,16 +3,23 @@ import java.util.List;
 
 public class MergeInterval{
     public static void main(String args[]){
-        int interval[][]={{1,3},{2,6},{8,10},{15,18}};
+        int nums[][]={{1,3},{2,6},{8,10},{15,18}};
         List<int[]> answer=new ArrayList<>();
+        answer.add(new int[]{nums[0][0],nums[0][1]});
+        for(int i=1;i<nums.length;i++){
+          int[] last=answer.get(answer.size()-1);
+          int interval[]=nums[i];
+          if(last[1]>=interval[0]){
+            last[0]=Math.min(interval[0],last[0]);
+            last[1]=Math.max(interval[1],last[1]);
+          }
+          else{
+            answer.add(new int[]{nums[i][0],nums[i][1]});
+          }
+        }
 
-        for(int i=1;i<interval.length;i++){
-            if(interval[i-1][1]>=interval[i][0] ){
-                interval[i-1][1]=Math.max(interval[i][1],interval[i-1][1]);
-            }
-            else if(interval[i-1][0]>=interval[i][0] && interval[i-1][1]<=interval[i][1] ||interval[i][0]>=interval[i-1][0] && interval[i][1]<=interval[i-1][1] ){
-                   
-            }
+        for(int e[]:answer){
+            System.out.print("["+e[0]+", "+e[1]+"]");
         }
     }
 }
